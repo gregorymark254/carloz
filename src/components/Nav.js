51481c2f6c91096315098dcbff83ec03a5529864
gregorymark254 <98938152+gregorymark254@users.noreply.github.com>
2022-11-10 10:38:20 +0300

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import {FaFacebookF,FaTwitter,FaInstagram,FaWhatsapp} from 'react-icons/fa'
+import {FaFacebookF,FaInstagram, FaWhatsapp} from 'react-icons/fa'
+import { VscGlobe } from "react-icons/vsc";
 
 const Nav = () => {
+
   return (
     <main>
        <div className="container mx-auto">
@@ -9,19 +11,18 @@ const Nav = () => {
           {/* Top nav */}
           <div className="md:flex divide-x-2 p-3 space-x-3 content-center place-items-end">
             <div>
-             <h3>John Carloz</h3>
+              <h3>John Carloz</h3>
             </div>
             <br />
             <div className="pl-5">
               <p>Photographer portfolio</p>
             </div>
           </div>
-    
           <div className='hidden md:flex'>
             <a href="https://www.facebook.com/profile.php?id=100008989771011" className="p-2 hover:text-blue-700"><FaFacebookF/></a>
             <a href="https://instagram.com/_royal.lens.ke?utm_media=copy_link" className="p-2 hover:text-indigo-700"><FaInstagram/></a>
-            <a href="https://wa.me/+254111522255" className="p-2 hover:text-green-600"><FaWhatsapp/></a>
-            <a href="http://royallens.vercel.app/" className="p-2 hover:text-indigo-700"><VscGlobe/></a>
+            <a href="https://wa.me/+254111522255" className="hover:text-green-600 p-2"><FaWhatsapp/></a>
+            <a className="hover:text-indigo-700 p-2" href="http://royallens.vercel.app/"><VscGlobe/></a>
           </div>
         </nav>
       </div>
@@ -30,3 +31,6 @@ const Nav = () => {
 }
 
 export default Nav
+
+
+         
